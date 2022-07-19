6ed7493ba4f4e6cdd827907abb5b7214766490b9
Homework1 <danisholha760@gmail.com>
2022-07-19 03:13:41 -0500

    third commit

--- src/Main.java
+++ src/Main.java
@@ -37,6 +37,43 @@ public class Main {
             System.out.println(year + "it's not the leap year");
         }
 
+        // Task 4
 
+        int deliveryDistance = 95;
+        if (deliveryDistance <= 20) {
+            System.out.println("Delivery will take 1 day");
+        } else if (deliveryDistance >= 20 && deliveryDistance <= 60) {
+            System.out.println("Delivery will take 2 day");
+        } else if (deliveryDistance >= 60 && deliveryDistance <= 100) {
+            System.out.println("Delivery will take 3 day");
+        } else {
+            System.out.println(" No delivery");
+        }
+
+        // Task 5
+        int monthNumber = 12;
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Winter");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Spring");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Summer");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Autumn");
+            default:
+                System.out.println("Doesn't exist");
+        }
     }
 }
